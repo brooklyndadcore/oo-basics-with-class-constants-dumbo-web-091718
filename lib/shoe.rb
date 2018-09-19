048b0092e0dcc00cc70e67bf14e1fd2621195796
brooklyndadcore <scott.waring@gmail.com>
2018-09-19 17:09:12 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.count == 0
+      BRANDS << @brand
+    elsif BRANDS[-1] != @brand
+      BRANDS << @brand
+    end
   end
 
   def cobble
